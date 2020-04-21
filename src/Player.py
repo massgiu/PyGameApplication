a508fd256acd32aa0474125cc7c96c3d6064079b
Massimiliano Giunchi <massimiliano.giunchi@gmail.com>
2020-04-21 19:34:12 +0200

    Modified position of player: or is on the left side or in the right

--- src/Player.py
+++ src/Player.py
@@ -15,6 +15,7 @@ class Player:
         self.run = True
         self.right = False
         self.left = False
+        self.isWalking = False
         self.walkCount = 0
         self.jumpCount = Utils.initCount
 
@@ -23,33 +24,39 @@ class Player:
         if self.walkCount + 1 >= Utils.clockTick:
             self.walkCount = 0
         # we need to select in the array walkLeft e walkRight the index
-        if self.left:  # facing left
-            win.blit(self.utils.walkLeft[self.walkCount // int(Utils.clockTick / len(Utils.img_list))],
-                     (self.init_x, self.init_y))  # We integer divide walkCount by 3 to ensure each
-            self.walkCount += 1  # image is shown 3 times every animation
-        elif self.right:  # facing right
-            win.blit(self.utils.walkRight[self.walkCount // int(Utils.clockTick / len(Utils.img_list))],
-                     (self.init_x, self.init_y))
+        if  (self.isWalking):
+            if self.left:  # facing left
+                win.blit(self.utils.walkLeft[self.walkCount // int(Utils.clockTick / len(Utils.img_list))],
+                         (self.init_x, self.init_y))  # We integer divide walkCount by a k(Utils.clockTick / len(Utils.img_list)) to ensure each
+                                                    # image is shown k times every animation
+            elif self.right:  # facing right
+                win.blit(self.utils.walkRight[self.walkCount // int(Utils.clockTick / len(Utils.img_list))],
+                         (self.init_x, self.init_y))
             self.walkCount += 1
+        #if it's not walking, loads first image
         else:
-            win.blit(Utils.char, (self.init_x, self.init_y))  # If the character is standing still
+            if self.right: #if is turned on right
+                win.blit(self.utils.walkRight[0], (self.init_x, self.init_y))  # If the character is standing still
+            else: #if is turned on left
+                win.blit(self.utils.walkLeft[0], (self.init_x, self.init_y))
         # pygame.draw.rect(win, (255, 0, 0), (init_x, init_y, charact_width, charact_height))
         pygame.display.update()
 
     def goLeft(self):
-        self.init_x -= Utils.vel
+        self.init_x -= Utils.vel #decrement x
         self.left = True
         self.right = False
+        self.isWalking = True
 
     def goRight(self):
-        self.init_x += Utils.vel
+        self.init_x += Utils.vel #increment x
         self.left = False
         self.right = True
+        self.isWalking = True
 
     def isStopped(self):
-        self.left = False
-        self.right = False
         self.walkCount = 0
+        self.isWalking = False
 
     def stopJump(self):
         self.isJump = True
