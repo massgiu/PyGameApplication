b143ede6cda6882201cf9c918fcec75615c684f4
Massimiliano Giunchi <massimiliano.giunchi@gmail.com>
2020-04-26 12:05:46 +0200

    Changed player sprites.

--- src/Utils.py
+++ src/Utils.py
@@ -3,7 +3,7 @@ import pygame
 class Utils:
     #Class attributes (static)
     init_pos_x = 100
-    init_pos_y = 580# 410
+    init_pos_y = 570# 410
     enemy_init_pos_x = init_pos_x+100
     enemy_init_pos_y = init_pos_y
     screen_width = 990 #500
@@ -17,10 +17,12 @@ class Utils:
     enemyVel = 3
     numMaxBullet = 5
     enemy_health = 10
-    img_list = [x for x in range(10) if x > 0]
+    img_list = [x for x in range(15) if x > 0]
     img_list_enemy = [x for x in range(11) if x > 0]
     walkRight = [pygame.image.load('../media/R' + str(x) + '.png') for x in img_list]
+    walkRight = [pygame.transform.scale(x, (60, 70)) for x in walkRight]
     walkLeft = [pygame.image.load('../media/L' + str(x) + '.png') for x in img_list]
+    walkLeft = [pygame.transform.scale(x, (60, 70)) for x in walkLeft]
     walkRightE = [pygame.image.load('../media/R' + str(x) + 'E.png') for x in img_list_enemy]
     walkLeftE = [pygame.image.load('../media/L' + str(x) + 'E.png') for x in img_list_enemy]
     clockTick = int(len(img_list)) * 3  # higher is factor, higher is speed animation
